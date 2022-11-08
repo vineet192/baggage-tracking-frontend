c0c711570803de698487478f22434051d1dc8a93
PranavBP <bellam.pranav@gmail.com>
2022-11-08 13:20:55 -0500

    Added routing according for Boarding Offical and Login

--- src/js/index.js
+++ src/js/index.js
@@ -3,177 +3,205 @@ import app from './static/env.js'
 const web3 = new Web3(app.smartContractUrl)
 
 let contract = new web3.eth.Contract([
-    {
-        "inputs": [
-            {
-                "internalType": "string",
-                "name": "baggageId",
-                "type": "string"
-            }
-        ],
-        "name": "addBaggageToBoarding",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "string",
-                "name": "baggageId",
-                "type": "string"
-            }
-        ],
-        "name": "addBaggageToSecurity",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "address[]",
-                "name": "officials",
-                "type": "address[]"
-            }
-        ],
-        "name": "assignBaggageOffcial",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "string[]",
-                "name": "baggageIds",
-                "type": "string[]"
-            },
-            {
-                "internalType": "string[]",
-                "name": "locations",
-                "type": "string[]"
-            }
-        ],
-        "name": "checkInBaggage",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "string[]",
-                "name": "customer_ids",
-                "type": "string[]"
-            }
-        ],
-        "stateMutability": "nonpayable",
-        "type": "constructor"
-    },
-    {
-        "inputs": [],
-        "name": "getBaggage",
-        "outputs": [
-            {
-                "components": [
-                    {
-                        "internalType": "string",
-                        "name": "id",
-                        "type": "string"
-                    },
-                    {
-                        "internalType": "uint256",
-                        "name": "last_scanned_timestamp",
-                        "type": "uint256"
-                    },
-                    {
-                        "internalType": "string",
-                        "name": "location",
-                        "type": "string"
-                    },
-                    {
-                        "internalType": "enum BaggageTracker.BaggageStatus",
-                        "name": "status",
-                        "type": "uint8"
-                    }
-                ],
-                "internalType": "struct BaggageTracker.Baggage[]",
-                "name": "",
-                "type": "tuple[]"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "string",
-                "name": "baggageId",
-                "type": "string"
-            }
-        ],
-        "name": "getBaggageStatus",
-        "outputs": [
-            {
-                "internalType": "enum BaggageTracker.BaggageStatus",
-                "name": "",
-                "type": "uint8"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "getCustomers",
-        "outputs": [
-            {
-                "components": [
-                    {
-                        "internalType": "string",
-                        "name": "id",
-                        "type": "string"
-                    },
-                    {
-                        "internalType": "enum BaggageTracker.CustomerStatus",
-                        "name": "status",
-                        "type": "uint8"
-                    }
-                ],
-                "internalType": "struct BaggageTracker.Customer[]",
-                "name": "",
-                "type": "tuple[]"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    }
-], "0xB9116249CD768b0ED7AEEBC9DdaE79a45b285eb6")
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "baggageId",
+				"type": "string"
+			}
+		],
+		"name": "addBaggageToBoarding",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "baggageId",
+				"type": "string"
+			}
+		],
+		"name": "addBaggageToSecurity",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address[]",
+				"name": "officials",
+				"type": "address[]"
+			}
+		],
+		"name": "assignBaggageOffcial",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string[]",
+				"name": "baggageIds",
+				"type": "string[]"
+			},
+			{
+				"internalType": "string[]",
+				"name": "locations",
+				"type": "string[]"
+			}
+		],
+		"name": "checkInBaggage",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string[]",
+				"name": "customer_ids",
+				"type": "string[]"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "getBaggage",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "string",
+						"name": "id",
+						"type": "string"
+					},
+					{
+						"internalType": "uint256",
+						"name": "last_scanned_timestamp",
+						"type": "uint256"
+					},
+					{
+						"internalType": "string",
+						"name": "location",
+						"type": "string"
+					},
+					{
+						"internalType": "enum BaggageTracker.BaggageStatus",
+						"name": "status",
+						"type": "uint8"
+					}
+				],
+				"internalType": "struct BaggageTracker.Baggage[]",
+				"name": "",
+				"type": "tuple[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "baggageId",
+				"type": "string"
+			}
+		],
+		"name": "getBaggageStatus",
+		"outputs": [
+			{
+				"internalType": "enum BaggageTracker.BaggageStatus",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getCustomers",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "string",
+						"name": "id",
+						"type": "string"
+					},
+					{
+						"internalType": "enum BaggageTracker.CustomerStatus",
+						"name": "status",
+						"type": "uint8"
+					}
+				],
+				"internalType": "struct BaggageTracker.Customer[]",
+				"name": "",
+				"type": "tuple[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getIsBoardingOfficial",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+], "0xBE89BC93CDD81ffEEFF9b46e71391968AE4042b9")
 
+web3.eth.getAccounts().then(console.log)
 contract.methods.getCustomers().call().then(console.log)
 contract.methods.getBaggage().call().then(console.log)
-//uncomment this method after updating the abi
-//contract.methods.isBoardingOfficial().call().then(console.log)
-console.log(contract.methods)
-console.log(contract)
 
-
+//contract.methods.checkInBaggage(["001"],["0000"]).call().then(console.log)
 
 async function connect() {
     if (typeof window.ethereum != undefined) {
-        console.log("We can see Metamask")
+        console.log("We can see Metamask Wallet")
         let user = await ethereum.request({ method: "eth_requestAccounts" });
-        console.log(user[0])
-        //web3.eth.getAccounts().then(console.log)
+        console.log(user)
     }
 }
 
-document.getElementById('connectbtn').addEventListener('click', () => {
-    connect()
-})
+async function getPersonAddress() {
+    if (typeof window.ethereum != undefined) {
+        let user = await ethereum.request({ method: "eth_requestAccounts" });
+        return user[0]
+    }
+}
 
-// async function execute() {
+document.getElementById('connectbtn').addEventListener('click', async () => {
+    await connect()
+
+    getPersonAddress().then(async (address) => {
+        console.log("got the address", address, typeof(address))
+        contract.methods.getIsBoardingOfficial().call({from : address})
+        .then(isOfficial => {
+            console.log("IsOfficial : ", isOfficial)
     
-// }
+            if(isOfficial){
+                window.location.href = app.serverUrl + "/src/boardingOfficial.html"
+            }
+            else{
+                window.location.href = app.serverUrl + "/src/login.html"
+            }
+        })
+    })
+
+})
--- src/js/static/env.js
+++ src/js/static/env.js
@@ -1,6 +1,6 @@
 var app = {
     smartContractUrl: "http://127.0.0.1:7545",
-    serverUrl: "http://localhost:5000"
+    serverUrl: "http://localhost:5500"
 }
 
 export default app 
