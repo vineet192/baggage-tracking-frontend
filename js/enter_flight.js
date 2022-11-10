ea0142aa7f6ec5fc290a59a52c4d29553790c651
Vineet Kalghatgi <vkalghat@gmail.com>
2022-11-10 17:50:44 -0500

    add flight verification and dynamic baggage loading

--- js/enter_flight.js
+++ js/enter_flight.js
@@ -1,10 +1,30 @@
 import app from './static/env.js'
 
-document.querySelector("form").addEventListener("submit", (event) => {
+document.querySelector("form").addEventListener("submit", async (event) => {
     event.preventDefault();
 
     let flight_no = document.getElementById("flight_no").value;
+
+    let isFlightVerified = await verify_flight(flight_no)
+
+    if (!isFlightVerified) {
+        return;
+    }
+
     sessionStorage.setItem("flight_no", flight_no);
 
     window.location.href = app.hostUrl + "/customer.html"
 })
+
+async function verify_flight(flight_no) {
+    let response;
+
+    response = await fetch(app.serverUrl + `/verify_flight?flight_no=${flight_no}`)
+
+    if(response.status === 200){
+        return true
+    }
+
+    return true
+
+}
