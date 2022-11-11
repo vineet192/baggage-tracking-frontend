1661cb82b10ceece93f2ffaa811ad304ea93ce1f
Vineet Kalghatgi <vkalghat@gmail.com>
2022-11-11 02:14:10 -0500

    add baggage scan functionality

---
+++ js/baggageOfficial.js
@@ -0,0 +1,18 @@
+import QrScanner from '../js/qr-code-scanner/qr-scanner-min.js';
+import app from '../js/static/env.js'
+
+let videoElem = document.querySelector("video")
+const qrScanner = new QrScanner(videoElem, result => { 
+    
+    console.log('decoded qr code:', result) 
+
+    sessionStorage.setItem("scanned-baggage-id", result)
+
+    window.location.href = app.hostUrl + "/scanBaggage.html"
+
+    qrScanner.stop();
+});
+
+document.getElementById("start_scan").addEventListener('click', (event) => {
+    qrScanner.start();
+})
