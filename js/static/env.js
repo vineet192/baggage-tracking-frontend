c9026e40d0bc6574901d03f38e3a772f145ab95c
Vineet Kalghatgi <vkalghat@gmail.com>
2022-12-07 17:25:36 -0500

    update smart contract address, remove smart contract url

--- js/static/env.js
+++ js/static/env.js
@@ -1,7 +1,7 @@
 var app = {
     smartContractUrl: "https://goerli.infura.io/v3/d18b86f3c6634fec9b1714b124ed59d9",
     hostUrl : 'http://127.0.0.1:5500',
-    serverUrl: "http://127.0.0.1:3000"
+    serverUrl: "https://baggage-tracking-backend.herokuapp.com"
 }
 
 export default app 
